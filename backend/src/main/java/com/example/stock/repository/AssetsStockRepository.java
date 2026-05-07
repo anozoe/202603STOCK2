90cb74d0b4f8a837a3dc5b114ceac9ea8297ced7
KeisukeYunoki <qifuyoumu@gmail.com>
2026-05-07 09:49:12 +0900

    Revert "Merge pull request #5 from anozoe/repository"
    This reverts commit 4dd4e5649d56661b3bac797543158ff42a90d4fb, reversing
    changes made to dd9fb4e51d2c70d4bdce5d68130aa34fde14e9a1.

--- backend/src/main/java/com/example/stock/repository/AssetsStockRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.stock.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssetsStockRepository extends JpaRepository<AssetsStockRepository, Integer> {
-    //TODO:保有銘柄一覧取得API
-    //TODO: 約定情報取得API_各銘柄の情報を取得
-}
--- backend/src/main/java/com/example/stock/repository/AssetsTotalHistoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.stock.repository;
-
-public class AssetsTotalHistoryRepository {
-    //TODO:総資産推移取得API_fridayJudge=1取得
-    //TODO: 資産履歴追加API_fridayJudge&assets_total登録
-}
